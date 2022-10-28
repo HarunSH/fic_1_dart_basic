98dcfa644c18f90c704cc2b4c057895fb170501f
Surya <suryaorenz@gmail.com>
2022-10-28 14:33:53 +0700

    init push github

---
+++ bin/fic_1_dart_basic.dart
@@ -0,0 +1,5 @@
+import 'package:fic_1_dart_basic/fic_1_dart_basic.dart' as fic_1_dart_basic;
+
+void main(List<String> arguments) {
+  print('Hello world: ${fic_1_dart_basic.calculate()}!');
+}
---
+++ test/fic_1_dart_basic_test.dart
@@ -0,0 +1,8 @@
+import 'package:fic_1_dart_basic/fic_1_dart_basic.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
